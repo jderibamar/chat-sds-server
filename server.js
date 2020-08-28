56f2e21d63392969cfb83b94f2bb47cfcb250741
jderibamar <jjrrdd@gmail.com>
2020-08-28 16:35:57 -0300

    CORS adicionado no server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // Setup basic express server
-var express = require('express')
-var app = express()
-var path = require('path')
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const path = require('path')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const cors = require('cors')
+const port = process.env.PORT || 3000
 
 server.listen(port, () => { console.log('Server listening at port %d', port) })
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')) )
+app.use(cors())
 
 // Chatroom
 
